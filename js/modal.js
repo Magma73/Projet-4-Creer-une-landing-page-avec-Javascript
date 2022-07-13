b4b1d614b60a65fdfc7862f8540119c350e2c81a
Marine <marine.magnin73@gmail.com>
2022-07-13 16:07:58 +0200

    Organisation : modification de la structure des fichiers

--- js/modal.js
+++ js/modal.js
@@ -11,9 +11,14 @@ function editNav() {
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
+const closeSpan = document.querySelector(".close");
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
+
+closeSpan.forEach((span) => span.addEventListener("click", closeModal));
+
+
 
 // launch modal form
 function launchModal() {
@@ -21,3 +26,6 @@ function launchModal() {
 }
 
 
+function closeModal(){
+  document.close();
+}
