bfb37a1b5b514ee31c685ecbb3a5f4241f64375f
Marine <marine.magnin73@gmail.com>
2022-07-13 22:03:45 +0200

    Issue n° 1 : ajouter la fonctionnalité au bouton (x)

--- js/modal.js
+++ js/modal.js
@@ -11,21 +11,22 @@ function editNav() {
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
-const closeSpan = document.querySelector(".close");
+const  modalCloseBtn = document.querySelector(".close");
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
 
-closeSpan.forEach((span) => span.addEventListener("click", closeModal));
 
+// Close modal form
+function closeModal() {
+  modalbg.style.display = "none";
+}
+
+// Close modal event
+modalCloseBtn.addEventListener ("click", closeModal);
 
 
 // launch modal form
 function launchModal() {
   modalbg.style.display = "block";
-}
-
-
-function closeModal(){
-  document.close();
 }
