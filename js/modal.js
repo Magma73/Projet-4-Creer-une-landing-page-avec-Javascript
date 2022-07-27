140acbd40e9784d88fe1c0d9a2c2a0a0a1618777
Marine <marine.magnin73@gmail.com>
2022-07-27 15:38:04 +0200

    Fonctionnalité : création des fonctions checkFirstNameInput et checkLastNameInput

--- js/modal.js
+++ js/modal.js
@@ -8,20 +8,15 @@ const firstName = document.getElementById("first");
 const lastName = document.getElementById("last");
 const email = document.getElementById("email");
 const birthdate = document.getElementById("birthdate");
-const quantity = document.getElementById("quantity");
-const radioInput =  document.querySelectorAll( 'input[type=radio]');
+const number = document.getElementById("quantity");
+const radioInput = document.querySelectorAll("input[type=radio]");
 const checkbox1 = document.getElementById("checkbox1");
-const checkbox2 = document.getElementById("checkbox2");
+// const checkbox2 = document.getElementById("checkbox2");
 
-const location1 = document.getElementById("location1");
-const location2 = document.getElementById("location2");
-const location3 = document.getElementById("location3");
-const location4 = document.getElementById("location4");
-const location5 = document.getElementById("location5");
-const location6 = document.getElementById("location6");
 
 
 // EVENTS
+
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
 
@@ -29,158 +24,123 @@ modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
 modalCloseBtn.addEventListener("click", closeModal);
 
 // FUNCTIONS
+
 // edit nav (media querie : mobile)
 function editNav() {
-    var x = document.getElementById("myTopnav");
-    if (x.className === "topnav") {
-        x.className += " responsive";
-    } else {
-        x.className = "topnav";
-    }
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
 }
 
 // launch modal form
 function launchModal() {
-    modalbg.style.display = "block";
+  modalbg.style.display = "block";
 }
 
 // close modal form
 function closeModal() {
-    modalbg.style.display = "none";
+  modalbg.style.display = "none";
 }
 
-// Validation form
+/****FUNCTION VALIDATION FORM****/
 
-// function validate(e){
-//     e.preventDefault();
+// Function verification Firstname
 
-//if (firstNameValidate == true) && (firstNameValidate == true)  {
-//else return false
+function checkFirstNameInput (firstName) {
 
-// const formDataTest = new FormData(formElem);
-// console.log(formDataTest);
-// console.log(formDataTest.get('first'));
-//   if(){
-//     prenom = true;
-//   }
-//   else{
-
-//   }
-//  }
-
-// formData.get('username');
-
-
-
-
-
-// Const verification Firstname
-const checkFirstNameInput = () => {
-  firstName.addEventListener("input", function (e) {
-      var value = e.target.value;
-      if (value.length > 2 && /^[a-zA-Z\s\-À-ÖØ-öø-ÿ]+$/.test(value)) {
-          isValid = true;
-          console.log("c'est valide");
-      } else {
-          isValid = false;
-          console.log("c'est pas bon");
-      }
-  });
+  if (firstName.value.length >= 2 && /^[a-zA-Z\s\-À-ÖØ-öø-ÿ]+$/.test(firstName.value)) {
+    console.log("c'est valide");
+    return true;
+  } else {
+    console.log("c'est pas bon");
+    return false;
+  }
 }
 
+// Function verification Lastname
 
-// Const verification Lastname
-  const checkLastNameInput = () => {
-  lastName.addEventListener("input", function (e) {
-      var value = e.target.value;
-      if (value.length > 2 && /^[a-zA-Z\s\-À-ÖØ-öø-ÿ]+$/.test(value)) {
-          isValid = true;
-          console.log("c'est valide");
-      } else {
-          isValid = false;
-          console.log("c'est pas bon");
-      }
-  });
+function checkLastNameInput (lastName) {
+
+  if (lastName.value.length >= 2 && /^[a-zA-Z\s\-À-ÖØ-öø-ÿ]+$/.test(lastName.value)) {
+    console.log("c'est valide");
+    return true;
+  } else {
+    console.log("c'est pas bon");
+    return false;
+  }
+
 }
 
-// Const verification Email
+// Function verification Email
+
 const checkEmailInput = () => {
-  email.addEventListener("input", function (e) {
-      var value = e.target.value;
-      if (/^((?!\.)[\w_.]*[^.])(@\w+)(\.\w+(\.\w+)?[^.\W])$/.test(value)) {
-          isValid = true;
-          console.log("c'est valide");
-      } else {
-          isValid = false;
-          console.log("c'est pas bon");
-      }
-  });
-}
+  const isEmailValid = email.value;
+
+  if (/^((?!\.)[\w_.]*[^.])(@\w+)(\.\w+(\.\w+)?[^.\W])$/.test(isEmailValid)) {
+    console.log("c'est valide");
+  } else {
+    console.log("c'est pas bon");
+  }
+  return isEmailValid;
+};
 
 // Const verification Birthdate
+
 const checkBirthdateInput = () => {
-  birthdate.addEventListener("input", function (e) {
-      var value = e.target.value;
-      if (/^[0-9]{4}[-]{1}[0-9]{2}[-]{1}[0-9]{2}$/.test(value)) {
-          isValid = true;
-          console.log("c'est valide");
-      } else {
-          isValid = false;
-          console.log("c'est pas bon");
-      }
-  });
-}
+  const isBirthdateValid = birthdate.value;
+
+  if (/^[0-9]{4}[-]{1}[0-9]{2}[-]{1}[0-9]{2}$/.test(isBirthdateValid)) {
+    console.log("c'est valide");
+  } else {
+    console.log("c'est pas bon");
+  }
+  return isBirthdateValid;
+};
 
 // Const verification Number
+
 const checkNumberInput = () => {
-  quantity.addEventListener("input", function (e) {
-      var value = e.target.value;
-      if (/^0*?[0-9]\d*$/.test(value)) {
-          isValid = true;
-          console.log("c'est valide");
-      } else {
-          isValid = false;
-          console.log("c'est pas bon");
-      }
-  });
- }
+  const isNumberValid = number.value;
 
-
-
-
-
+  if (/^0*?[0-9]\d*$/.test(isNumberValid)) {
+    console.log("c'est valide");
+  } else {
+    console.log("c'est pas bon");
+  }
+  return isNumberValid;
+};
 
 // Bouton radio
 
-for (i=0 ; i<radioInput.length ; i++) {
-  radioInput[i].addEventListener("change", function(e) {
-    var value = e.target;
-      if (value.checked){
-          console.log("Bouton coché !");
-      }
-      else{
-        console.log("Vous n'avez pas coché de bouton");
-      }
-    });
-}
+// for (i = 0; i < radioInput.length; i++) {
+//     radioInput[i].addEventListener("change", function (e) {
+//         var value = e.target;
+//         if (value.checked) {
+//             console.log("Bouton coché !");
+//         } else {
+//             console.log("Vous n'avez pas coché de bouton");
+//         }
+//     });
+// }
 
+// let choix;
 
-  // let choix;
-
-  // for (let radios of radioInput)
-  // {
-  //     radioInput.length.addEventListener('click', function(e) {
-  //   if (radios.checked){
-  //       choix = radios.value;
-  //       console.log("Bouton coché !");
-  //   }
-  //   else{
-  //     console.log("Vous n'avez pas coché de bouton");
-  //   }
-  //   return choix;
-  // });
-  // }
-
+// for (let radios of radioInput)
+// {
+//     radioInput.length.addEventListener('click', function(e) {
+//   if (radios.checked){
+//       choix = radios.value;
+//       console.log("Bouton coché !");
+//   }
+//   else{
+//     console.log("Vous n'avez pas coché de bouton");
+//   }
+//   return choix;
+// });
+// }
 
 // function radioValidate() {
 //   let choix;
@@ -221,17 +181,111 @@ for (i=0 ; i<radioInput.length ; i++) {
 //   }
 // }
 
+function checkRadioInput (radioInput) {
+  // let radio = radioInput;
+
+  // radio.onclick = (e) => {
+  //   let choix;
+
+  //   for (let radios of radioInput)
+  //   {
+  //     if (radios.checked){
+  //         choix = radios.value;
+  //         console.log("Bouton coché !");
+  //     }
+  //     else{
+  //       console.log("Vous n'avez pas coché de bouton");
+  //     }
+  //     return choix;
+  //   }
+
+  // }
+  let isCheckRadioValid = 0;
+
+  // for (i = 0; i < radioInput.length; i++) {
+  //   radioInput[i].addEventListener("change", function (e) {
+  //     var value = e.target;
+  //     if (value.checked) {
+  //       console.log("Bouton coché !");
+  //       console.log(value);
+  //     } else {
+  //       console.log("Vous n'avez pas coché de bouton");
+  //     }
+  //   });
+  // }
+
+  // return value;
+
+  radioInput.foreach (radios=>
+  {
+
+    if (radios.checked){
+        isCheckRadioValid = 1;
+    }
+  })
+
+  if(isCheckRadioValid === 0){
+    console.log("Vous n'avez pas coché de bouton");
+    return false;
+  }
+  else{
+    console.log("Bouton coché !");
+    console.log(radios.value);
+    return true;
+  };
+}
+
 
 
 // Const verification Conditions générales
+
 const checkBoxInput = () => {
-checkbox1.addEventListener('change', function() {
+  const isCheckBoxValid = checkbox1.value;
+
   if (this.checked) {
     console.log("C'est checké");
   } else {
     console.log("C'est pas checké");
   }
-});
+  return isCheckBoxValid;
+};
+
+// Const validation form
+
+const isFormValid = () => checkFirstNameInput() && checkLastNameInput() && checkEmailInput() && checkBirthdateInput() && checkNumberInput() && checkRadioInput() && checkBoxInput();
+
+// Validate form
+
+function validate(e) {
+  e.preventDefault();
+
+  const formData = new FormData(form);
+    const values = [...formData.entries()];
+
+
+  if (isFormValid()) {
+    window.alert("Merci ! Votre réservation a été reçue.");
+    console.log(values);
+  }
+  else{
+    window.alert("Vous devez remplir le formulaire.");
+    return false;
+  }
 }
+
+//if (firstNameValidate == true) && (firstNameValidate == true)  {
+//else return false
+
+// const formDataTest = new FormData(formElem);
+// console.log(formDataTest);
+// console.log(formDataTest.get('first'));
+//   if(){
+//     prenom = true;
+//   }
+//   else{
+
+//   }
+
+// formData.get('username');
 
 
