6fa1013f6c09c35c0ee4d80851d389254720de29
Marine <marine.magnin73@gmail.com>
2022-07-29 12:42:51 +0200

    Structure et fonctionnalités : résolution de l'issue 3 => ajouter validation ou messages d'erreur

--- js/modal.js
+++ js/modal.js
@@ -48,9 +48,11 @@ function closeModal() {
 function checkFirstNameInput(firstName) {
   if (firstName.value.length >= 2 && /^[a-zA-Z\s\-À-ÖØ-öø-ÿ]+$/.test(firstName.value)) {
     console.log("c'est valide");
+    formData[0].setAttribute("data-error-visible", "false");
     return true;
   } else {
     console.log("c'est pas bon");
+    formData[0].setAttribute("data-error-visible", "true");
     return false;
   }
 }
@@ -59,9 +61,11 @@ function checkFirstNameInput(firstName) {
 function checkLastNameInput(lastName) {
   if (lastName.value.length >= 2 && /^[a-zA-Z\s\-À-ÖØ-öø-ÿ]+$/.test(lastName.value)) {
     console.log("c'est valide");
+    formData[1].setAttribute("data-error-visible", "false");
     return true;
   } else {
     console.log("c'est pas bon");
+    formData[1].setAttribute("data-error-visible", "true");
     return false;
   }
 }
@@ -70,9 +74,11 @@ function checkLastNameInput(lastName) {
 function checkEmailInput(email) {
   if (/^((?!\.)[\w_.]*[^.])(@\w+)(\.\w+(\.\w+)?[^.\W])$/.test(email.value)) {
     console.log("c'est valide");
+    formData[2].setAttribute("data-error-visible", "false");
     return true;
   } else {
     console.log("c'est pas bon");
+    formData[2].setAttribute("data-error-visible", "true");
     return false;
   }
 }
@@ -83,9 +89,11 @@ function checkBirthdateInput(birthdate) {
   let today = new Date();
   if (/^[0-9]{4}[-]{1}[0-9]{2}[-]{1}[0-9]{2}$/.test(birthdate.value) && birthdate.value < today.toISOString()) {
     console.log("c'est valide");
+    formData[3].setAttribute("data-error-visible", "false");
     return true;
   } else {
     console.log("c'est pas bon");
+    formData[3].setAttribute("data-error-visible", "true");
     return false;
   }
 }
@@ -95,9 +103,11 @@ function checkBirthdateInput(birthdate) {
 function checkQuantityInput(quantity) {
   if (/^0*?[0-9]\d*$/.test(quantity.value)) {
     console.log("c'est valide");
+    formData[4].setAttribute("data-error-visible", "false");
     return true;
   } else {
     console.log("c'est pas bon");
+    formData[4].setAttribute("data-error-visible", "true");
     return false;
   }
 }
@@ -115,9 +125,11 @@ function checkRadioInput(radioInput) {
 
   if (checkRadioValid === 0) {
     console.log("Vous n'avez pas coché de bouton");
+    formData[5].setAttribute("data-error-visible", "true");
     return false;
   } else {
     console.log("Bouton coché !");
+    formData[5].setAttribute("data-error-visible", "false");
     return true;
   }
 }
@@ -127,41 +139,14 @@ function checkRadioInput(radioInput) {
 function checkBoxInput(checkbox1) {
   if (checkbox1.checked) {
     console.log("C'est checké");
+    formData[6].setAttribute("data-error-visible", "false");
     return true;
   } else {
     console.log("C'est pas checké");
+    formData[6].setAttribute("data-error-visible", "true");
     return false;
   }
 }
-
-// Const validation form
-
-// const isFormValid = () => checkFirstNameInput() && checkLastNameInput() && checkEmailInput() && checkBirthdateInput() && checkNumberInput() && checkRadioInput() && checkBoxInput();
-
-// const isFormValid = () =>
-//   checkFirstNameInput(firstName) &&
-//   checkLastNameInput(lastName) &&
-//   checkEmailInput(email) &&
-//   checkBirthdateInput(birthdate) &&
-//   checkQuantityInput(quantity) &&
-//   checkRadioInput(radioInput) &&
-//   checkBoxInput(checkbox1);
-
-
-
-
-// if(
-//   checkFirstNameInput(firstName) &&
-//   checkLastNameInput(lastName) &&
-//   checkEmailInput(email) &&
-//   checkBirthdateInput(birthdate) &&
-//   checkQuantityInput(quantity) &&
-//   checkRadioInput(radioInput) &&
-//   checkBoxInput(checkbox1) === true
-// ){
-//   console.log("true");
-
-// }
 
 
 // Validate form
@@ -177,8 +162,10 @@ addEventListener('submit', (e) => {
     checkRadioInput(radioInput) &&
     checkBoxInput(checkbox1) === true
   ){
+    // document.getElementsByName("resserve").reset();
     window.alert("Merci ! Votre réservation a été reçue.");
-
+    // window.location.reload();
+    closeModal();
   }
   else{
     window.alert("Vous devez remplir le formulaire.");
